a620ec38b2a97af4a2d35d89fb83a3ba28ea5da3
jackedwards1128 <jack.edwards@menloschool.org>
2025-09-22 01:00:08 -0700

    Added weight balancing.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -23,52 +23,70 @@ public class HighwaysAndHospitals {
         // Create map to house the root of a given city
         int[] map_tree = new int[n+1];
 
-        // Array to store the cities we look at when searching for their root
-//        int[] path_to_root = new int[n];
-        int path_to_root = 0;
+        // For weight balancing, the size of a subgraph is represented as a negative number
+        for (int i = 0; i < n+1; i++) {
+            map_tree[i] = -1;
+        }
 
+        // Variable to store a city that isn't path-compressed when searching for their root
+        int path_to_root;
+
+        // For every potential highway
         for (int[] connection : cities) {
-            // find parent's root
-            int root_of_parent = connection[0];
+            // First number in connection will number one
+            int root_number_one = connection[0];
             path_to_root = -1;
-
-            while (map_tree[root_of_parent] != 0) {
-                path_to_root = root_of_parent;
-                root_of_parent = map_tree[root_of_parent];
+            // Find num one's root
+            while (map_tree[root_number_one] > 0) {
+                path_to_root = root_number_one;
+                root_number_one = map_tree[root_number_one];
             }
+            // If we found a non-path-compressed city along the way, compress it
             if (path_to_root != -1)
-                map_tree[path_to_root] = root_of_parent;
+                map_tree[path_to_root] = root_number_one;
 
-            // find child's root
-            int new_child = connection[1];
+
+            // Second number in connection is number two
+            int root_number_two = connection[1];
             path_to_root = -1;
-
-            while(map_tree[new_child] != 0) {
-//                path_to_root[i] = new_child;
-//                i++;
-                path_to_root = new_child;
-                new_child = map_tree[new_child];
+            // Find num two's root
+            while(map_tree[root_number_two] > 0) {
+                path_to_root = root_number_two;
+                root_number_two = map_tree[root_number_two];
             }
-
+            // If we found a non-path-compressed city along the way, compress it
             if(path_to_root != -1)
-                map_tree[path_to_root] = new_child;
+                map_tree[path_to_root] = root_number_two;
 
-            // check if same root
-            if (root_of_parent == new_child) {
+
+            // If num one and two share a root, skip
+            if (root_number_one == root_number_two) {
                 continue;
             }
 
-            map_tree[new_child] = connection[0];
+            // If num two's root has a smaller tree than num one's root
+            if (map_tree[root_number_two] > map_tree[root_number_one]) {
+                // Then num two will become a child of num one's root
+                map_tree[root_number_one] += map_tree[root_number_two];
+                map_tree[root_number_two] = root_number_one;
+            } else {
+                // Otherwise, num two will become the root of num one's tree
+                map_tree[root_number_two] += map_tree[root_number_one];
+                map_tree[root_number_one] = root_number_two;
+            }
         }
 
+        // Count ecosystems by counting empty spaces in the map array
         for (int city : map_tree) {
-            if (city == 0) {
+            if (city < 0) {
                 ecosystem_amount++;
             }
         }
 
+        // Don't count index zero as a city
         ecosystem_amount--;
 
+        // Calculate and return the final cost
         long final_cost = (ecosystem_amount * hospitalCost) + ((n - ecosystem_amount) * highwayCost);
 
         return final_cost;
